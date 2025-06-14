62bc0a39625831020fb06c7caa3cc8f88382de7a
Faisal-LTF <muh.faisalltf@gmail.com>
2025-06-15 00:53:13 +0800

    update tambahan pilihan event

--- resources/js/Pages/Landing/Index.vue
+++ resources/js/Pages/Landing/Index.vue
@@ -5,6 +5,54 @@ const smoothScroll = (id) => {
         behavior: 'smooth'
     });
 };
+
+import { ref, onMounted } from "vue";
+import { ProductService } from '@/sakai/service/ProductService';
+
+onMounted(() => {
+    ProductService.getProductsSmall().then((data) => (products.value = data.slice(0, 9)));
+})
+
+const products = ref();
+const responsiveOptions = ref([
+    {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1
+    },
+    {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1
+    }
+]);
+
+const getSeverity = (status) => {
+    switch (status) {
+        case 'INSTOCK':
+            return 'success';
+
+        case 'LOWSTOCK':
+            return 'warn';
+
+        case 'OUTOFSTOCK':
+            return 'danger';
+
+        default:
+            return null;
+    }
+};
+
 </script>
 
 <template>
@@ -195,52 +243,37 @@ const smoothScroll = (id) => {
 
             <div id="highlights" class="py-6 px-6 lg:px-20 mx-0 my-12 lg:mx-20">
                 <div class="text-center">
-                    <div class="text-surface-900 light:text-surface-0 font-normal mb-2 text-4xl">Powerful Everywhere
+                    <div class="text-surface-900 light:text-surface-0 font-normal mb-2 text-4xl">Event Spektakuler!
                     </div>
-                    <span class="text-muted-color text-2xl">Amet consectetur adipiscing elit...</span>
+                    <span class="text-muted-color text-2xl">Cari event dan pilih kandidat mu ....</span>
                 </div>
 
-                <div class="grid grid-cols-12 gap-4 mt-20 pb-2 md:pb-20">
-                    <div class="flex justify-center col-span-12 lg:col-span-6 bg-purple-100 p-0 order-1 lg:order-none"
-                        style="border-radius: 8px">
-                        <img src="/demo/images/landing/mockup.svg" class="w-11/12" alt="mockup mobile" />
-                    </div>
-
-                    <div
-                        class="col-span-12 lg:col-span-6 my-auto flex flex-col lg:items-end text-center lg:text-right gap-4">
-                        <div class="flex items-center justify-center bg-purple-200 self-center lg:self-end"
-                            style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
-                            <i class="pi pi-fw pi-mobile !text-4xl text-purple-700"></i>
-                        </div>
-                        <div class="leading-none text-surface-900 light:text-surface-0 text-3xl font-normal">Congue
-                            Quisque Egestas</div>
-                        <span class="text-surface-700 light:text-surface-100 text-2xl leading-normal ml-0 md:ml-2"
-                            style="max-width: 650px">Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Eget
-                            aliquet nibh praesent tristique magna sit amet purus gravida. Sit amet mattis vulputate enim
-                            nulla aliquet.</span>
-                    </div>
+                <div class="card">
+                    <Carousel :value="products" :numVisible="3" :numScroll="1" :responsiveOptions="responsiveOptions">
+                        <template #item="slotProps">
+                            <div class="border border-surface-200 dark:border-surface-700 rounded m-2  p-4">
+                                <div class="mb-4">
+                                    <div class="relative mx-auto">
+                                        <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image"
+                                            :alt="slotProps.data.name" class="w-full rounded" />
+                                        <Tag :value="slotProps.data.inventoryStatus"
+                                            :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute"
+                                            style="left:5px; top: 5px" />
+                                    </div>
+                                </div>
+                                <div class="mb-4 font-medium">{{ slotProps.data.name }}</div>
+                                <div class="flex justify-between items-center">
+                                    <div class="mt-0 font-semibold text-xl">${{ slotProps.data.price }}</div>
+                                    <span>
+                                        <Button icon="pi pi-heart" severity="secondary" outlined />
+                                        <Button icon="pi pi-shopping-cart" class="ml-2" />
+                                    </span>
+                                </div>
+                            </div>
+                        </template>
+                    </Carousel>
                 </div>
 
-                <div class="grid grid-cols-12 gap-4 my-20 pt-2 md:pt-20">
-                    <div
-                        class="col-span-12 lg:col-span-6 my-auto flex flex-col text-center lg:text-left lg:items-start gap-4">
-                        <div class="flex items-center justify-center bg-yellow-200 self-center lg:self-start"
-                            style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
-                            <i class="pi pi-fw pi-desktop !text-3xl text-yellow-700"></i>
-                        </div>
-                        <div class="leading-none text-surface-900 light:text-surface-0 text-3xl font-normal">Celerisque
-                            Eu Ultrices</div>
-                        <span class="text-surface-700 light:text-surface-100 text-2xl leading-normal mr-0 md:mr-2"
-                            style="max-width: 650px">Adipiscing commodo elit at imperdiet dui. Viverra nibh cras
-                            pulvinar mattis nunc sed blandit libero. Suspendisse in est ante in. Mauris pharetra et
-                            ultrices neque ornare aenean euismod elementum nisi.</span>
-                    </div>
-
-                    <div class="flex justify-end order-1 sm:order-2 col-span-12 lg:col-span-6 bg-yellow-100 p-0"
-                        style="border-radius: 8px">
-                        <img src="/demo/images/landing/mockup-desktop.svg" class="w-11/12" alt="mockup" />
-                    </div>
-                </div>
             </div>
             <div class="py-6 px-6 mx-0 mt-20 lg:mx-20">
                 <div class="grid grid-cols-12 gap-4">
